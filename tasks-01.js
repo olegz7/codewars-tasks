2405c7d7fde25b30df12205b11823ec76c3f0aec
olegz7 <76254867+olegz7@users.noreply.github.com>
2021-04-15 21:51:57 +0300

    added js tasks from codewars.com

--- tasks-01.js
+++ tasks-01.js
@@ -1,31 +1,40 @@
 // https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
 
-
-
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
 
 
 // https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
 
-
-
+function move(position, roll) {
+    return position + roll * 2;
+}
 
 
 // https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
 
-
+function greet(name, owner) {
+    if (name === owner) {
+        return "Hello boss";
+    }
+    return "Hello guest";
+}
 
 
 // https://www.codewars.com/kata/keep-hydrated-1/train/javascript
 
+function litres(time) {
+    return Math.floor(time * 0.5);
+}
 
 
-
-// Вчорашня задачка, вирішив через хвилин 15 після кінця уроку ))
+// Вчорашня задачка з манго сподобалася, вирішив її десь через хвилин 15 після кінця уроку ))
 //  https://www.codewars.com/kata/57a77726bb9944d000000b06
 
 function mango(quantity, price) {
-    const freeMango = Math.floor(quantity/3);
+    const freeMango = Math.floor(quantity / 3);
     const mangoToPay = quantity - freeMango;
     const totalCost = mangoToPay * price;
     return totalCost;
-  }
+}
