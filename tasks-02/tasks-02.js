5784f5c8db88e28b3d92c6c0a7f304360d3cadc6
olegz7 <76254867+olegz7@users.noreply.github.com>
2021-04-19 17:34:22 +0300

    Update tasks-02.js

--- tasks-02/tasks-02.js
+++ tasks-02/tasks-02.js
@@ -22,12 +22,12 @@ function DNAtoRNA(dna) {
 
 // https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
 
-var min = function(list){
+let min = function(list){
     list.sort( (a, b) =>  a - b );
     return list[0];
 }
 
-var max = function(list){
+let max = function(list){
     list.sort( (a, b) =>  b - a );
     return list[0];
 }
