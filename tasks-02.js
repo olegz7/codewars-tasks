eda16fb3c71cb40fb128d053669201ce74effac3
Oleg Zubkov <76254867+olegz7@users.noreply.github.com>
2021-05-19 22:16:59 +0300

    Update tasks-02.js

--- tasks-02.js
+++ tasks-02.js
@@ -20,6 +20,16 @@ function DNAtoRNA(dna) {
     return rna;
 }
 
+// --- more elegant way ---
+function DNAtoRNA(dna) {
+    const rnaArr = [];
+    for (let i = 0; i < dna.length; i++) {
+        rnaArr.push(dna[i] === "T" ? "U" : dna[i]);
+    }
+    return rnaArr.join("");
+}
+
+
 // https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
 
 let min = function(list){
@@ -32,8 +42,21 @@ let max = function(list){
     return list[0];
 }
 
+// --- better use Math.min / Math.max ---
+
+const min = function(list){
+    return Math.min(...list);
+}
+
+const max = function(list){
+    return Math.max(...list);
+}
+
 // https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
 
+function min(arr, toReturn) {
+  return toReturn === "value" ?  Math.min(...arr) : arr.indexOf(Math.min(...arr));
+}
 
 // Поглиблені задачки (робити за бажанням):
 // https://www.codewars.com/kata/a-wolf-in-sheeps-clothing/train/javascript
