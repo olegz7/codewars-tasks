87413db5cda84f46ba5228a75ea06d51c6c18f20
olegz7 <76254867+olegz7@users.noreply.github.com>
2021-05-22 11:49:35 +0300

    updated task with animals

--- tasks-03.js
+++ tasks-03.js
@@ -14,42 +14,29 @@ function circleCircumference(circle) {
 
 // https://www.codewars.com/kata/fun-with-es6-classes-number-2-animals-and-inheritance/train/javascript
 
-
 class Shark extends Animal {
-  constructor(name, age, status, legs, species) {
-    super(name, age);
-    this.status = status;
-    this.legs = 0;
-    this.species = "shark"
-  }
-  introduce() {
-    return `Hello, my name is ${this.name} and I am ${this.age} years old.`;
-  }
+    constructor(name, age, status) {
+        super(name, age, 0, "shark", status);
+    }
 }
 
 class Cat extends Animal {
-  constructor (name, age, status, legs, species) {
-    super(name, age);
-    this.status = status;
-    this.legs = 4;
-    this.species = "cat"
-  }
-  introduce() {
-    return `Hello, my name is ${this.name} and I am ${this.age} years old.  Meow meow!`;
-  }
+    constructor(name, age, status) {
+        super(name, age, 4, "cat", status);
+    }
+    introduce() {
+        return `${super.introduce()}  Meow meow!`;
+    }
 }
 
 class Dog extends Animal {
-  constructor (name, age, status, master, legs, species) {
-    super(name, age);
-    this.status = status;
-    this.master = master;
-    this.legs = 4;
-    this.species = "dog"
-  }
-  greetMaster() {
-    return `Hello ${this.master}`;
-  }
+    constructor(name, age, status, master) {
+        super(name, age, 4, "dog", status);
+        this.master = master;
+    }
+    greetMaster() {
+        return `Hello ${this.master}`;
+    }
 }
 
 
